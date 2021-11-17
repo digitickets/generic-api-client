57e9bf64be418d62a2269472d2f4e0eafb46822c
Anthony Kuske <anthonykuske@gmail.com>
2021-11-17 15:51:14 +0100

    Actually remove dotenv

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,16 +2,8 @@
 
 namespace GenericApiClientTests;
 
-use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
 
 class AbstractTestCase extends TestCase
 {
-    protected function setUp()
-    {
-        parent::setUp();
-
-        $dotenv = Dotenv::createImmutable(__DIR__.'/../');
-        $dotenv->load();
-    }
 }
